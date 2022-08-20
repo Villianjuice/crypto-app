f8bfdc0da1112d7cddcffcfc305d1228f15f050f
Aslan <tavorktar@gmail.com>
2022-08-21 02:25:02 +0600

    pagination

--- src/components/coinsTable/CoinsTable.tsx
+++ src/components/coinsTable/CoinsTable.tsx
@@ -1,4 +1,5 @@
-import { LinearProgress, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import React, { useState } from 'react'
+import { LinearProgress, Pagination, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
 import { createSelector } from '@reduxjs/toolkit'
 import { useNavigate } from 'react-router-dom'
 
@@ -13,10 +14,49 @@ interface CoinsTableProps {
   search: string
 }
 
+const sx = {
+  row: {
+    backgroundColor: "#16171a",
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: "#131111",
+    },
+    fontFamily: "Montserrat",
+  },
+  linear: {
+    backgroundColor: "gold"
+  },
+  tableHead: {
+    backgroundColor: "#EEBC1D"
+  },
+  tableCellCoin: {
+    display: 'flex', 
+    gap: '15px'
+  },
+  tableCellNames: {
+    color: "black",
+    fontWeight: "700",
+    fontFamily: "Montserrat",
+  },
+  pagination: {
+    padding: '25px',
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    "& .MuiPaginationItem-root": {
+      color: "gold",
+    },
+  },
+}
+
 export const CoinsTable: React.FC <CoinsTableProps> = ({search}) => {
+  const [page, setPage] = useState(1)
+
   const handleSearch = createSelector(
     (state: RootState) => state.coins,
-    (coins) => coins.coins.filter(coin => coin.name.toLowerCase().includes(search) || coin.symbol.toLowerCase().includes(search))
+    (coins) => coins.coins
+    .slice((page - 1) * 10, (page - 1) * 10 + 10)
+    .filter(coin => coin.name.toLowerCase().includes(search) || coin.symbol.toLowerCase().includes(search))
   )
 
   const loading = useAppSelector(state => state.coins.loading)
@@ -26,104 +66,90 @@ export const CoinsTable: React.FC <CoinsTableProps> = ({search}) => {
 
   const nav = useNavigate()
 
-  const sx = {
-    row: {
-      backgroundColor: "#16171a",
-      cursor: "pointer",
-      "&:hover": {
-        backgroundColor: "#131111",
-      },
-      fontFamily: "Montserrat",
-    },
-    linear: {
-      backgroundColor: "gold"
-    },
-    tableHead: {
-      backgroundColor: "#EEBC1D"
-    },
-    tableCellCoin: {
-      display: 'flex', 
-      gap: '15px'
-    },
-    tableCellNames: {
-      color: "black",
-      fontWeight: "700",
-      fontFamily: "Montserrat",
-    }
-  }
+  console.log(coins.length)
 
   return (
-    <TableContainer>
-      {loading && <LinearProgress sx={sx.linear} />}
-      {coins.length > 0 && 
-        <Table>
-        <TableHead sx={sx.tableHead}>
-          <TableRow>
-            {["Coin", "Price", "24h Change", "Market Cap"].map(head => 
-              <TableCell 
-                sx={sx.tableCellNames}
-                key={head}
-                align={head === "Coin" ? "inherit" : "right"}
-              >
-                {head}
-              </TableCell>  
-            )}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {coins.map(coin => {
-            const profit = coin.price_change_percentage_24h > 0;
-            
-            return (
-              <TableRow 
-                sx={sx.row}
-                key={coin.id}
-              >
+    <>
+      <TableContainer>
+        {loading && <LinearProgress sx={sx.linear} />}
+        {coins.length > 0 && 
+          <Table>
+          <TableHead sx={sx.tableHead}>
+            <TableRow>
+              {["Coin", "Price", "24h Change", "Market Cap"].map(head => 
                 <TableCell 
-                  onClick={() => nav('/coin/1')} 
-                  component='th' 
-                  scope='row' 
-                  sx={sx.tableCellCoin}
+                  sx={sx.tableCellNames}
+                  key={head}
+                  align={head === "Coin" ? "inherit" : "right"}
                 >
-                  <img src={coin.image} alt={coin.name} className={styles.coins__img} />
-                  <div className={styles.coins__about}>
-                    <span className={styles.coins__symbol}>{coin.symbol}</span>
-                    <span className={styles.coins__title}>{coin.name}</span>
-                  </div>
-                </TableCell>
-                
-                <TableCell align="right">
-                  {symbol}{" "}
-                  {RoundNumber(coin.current_price.toFixed(2))}
-                </TableCell>
+                  {head}
+                </TableCell>  
+              )}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {coins.map(coin => {
+              const profit = coin.price_change_percentage_24h > 0;
+              
+              return (
+                <TableRow 
+                  sx={sx.row}
+                  key={coin.id}
+                >
+                  <TableCell 
+                    onClick={() => nav('/coin/1')} 
+                    component='th' 
+                    scope='row' 
+                    sx={sx.tableCellCoin}
+                  >
+                    <img src={coin.image} alt={coin.name} className={styles.coins__img} />
+                    <div className={styles.coins__about}>
+                      <span className={styles.coins__symbol}>{coin.symbol}</span>
+                      <span className={styles.coins__title}>{coin.name}</span>
+                    </div>
+                  </TableCell>
+                  
+                  <TableCell align="right">
+                    {symbol}{" "}
+                    {RoundNumber(coin.current_price.toFixed(2))}
+                  </TableCell>
 
-                <TableCell
-                  align="right"
-                  sx={{
-                    color: profit ? "rgb(14, 203, 129)" : "red",
-                    fontWeight: 500,
-                  }}
-                >
-                  {profit && "+"}
-                  {coin.price_change_percentage_24h.toFixed(2)}%
-                </TableCell>
-                
-                <TableCell align="right">
-                  {symbol}{" "}
-                  {RoundNumber(
-                    coin.market_cap.toString().slice(0, -6)
-                  )}
-                  M
-                </TableCell>
-              </TableRow>
-            )
-            }
-          )}
-        </TableBody>
-        </Table>
-      }
-      {error && <h2>{error}</h2>}
-      
-    </TableContainer>
+                  <TableCell
+                    align="right"
+                    sx={{
+                      color: profit ? "rgb(14, 203, 129)" : "red",
+                      fontWeight: 500,
+                    }}
+                  >
+                    {profit && "+"}
+                    {coin.price_change_percentage_24h.toFixed(2)}%
+                  </TableCell>
+                  
+                  <TableCell align="right">
+                    {symbol}{" "}
+                    {RoundNumber(
+                      coin.market_cap.toString().slice(0, -6)
+                    )}
+                    M
+                  </TableCell>
+                </TableRow>
+              )
+              }
+            )}
+          </TableBody>
+          </Table>
+        }
+        {error && <h2>{error}</h2>}
+        
+      </TableContainer>
+      <Pagination  
+        count={coins.length}
+        sx={sx.pagination}
+        onChange={(_, value) => {
+          setPage(value);
+          window.scroll(0, 450);
+        }}
+      />
+    </>
   )
 }
